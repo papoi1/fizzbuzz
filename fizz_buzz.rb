ad0aff064a27c3c64238c990929cc12a28cee911
papoi1 <mikelxabier@gmail.com>
2018-08-02 17:17:06 +0200

    refactor mache and action

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,19 +1,17 @@
-class FizzBuzz
+class NonFizzBuzz
   class << self
-    FIZZ = "fizz"
-    BUZZ = "buzz"
     FIZZ_NUMBER = 3
     BUZZ_NUMBER = 5
+    def match?(number)
+      return !fizz?(number) && !buzz?(number)
+    end
 
-    def translate(number)
-      result = ""
-      result.prepend(FIZZ) if fizz?(number)
-      result << BUZZ  if buzz?(number)
-      result = number.to_s if !fizz?(number) && !buzz?(number)
-      return result
+    def action(number, result)
+      result << number.to_s
     end
 
     private
+
     def fizz?(number)
       divisible?(number, FIZZ_NUMBER)
     end
@@ -24,6 +22,63 @@ class FizzBuzz
 
     def divisible?(number, divider)
       return (number % divider == 0)
+    end
+  end
+end
+
+class Buzz
+  class << self
+    BUZZ_NUMBER = 5
+    BUZZ = "buzz"
+
+    def match?(number)
+      return divisible?(number, BUZZ_NUMBER)
+    end
+
+    def action(_, result)
+      result << BUZZ
+    end
+
+    private
+
+    def divisible?(number, divider)
+      return (number % divider == 0)
+    end
+  end
+end
+
+class Fizz
+  class << self
+
+    FIZZ = "fizz"
+    FIZZ_NUMBER = 3
+
+    def match?(number)
+      return divisible?(number, FIZZ_NUMBER)
+    end
+
+    def action(_, result)
+      result.prepend(FIZZ)
+    end
+
+    private
+
+    def divisible?(number, divider)
+      return (number % divider == 0)
+    end
+  end
+end
+
+
+class FizzBuzz
+  class << self
+    STRATEGIES = [Fizz, Buzz, NonFizzBuzz]
+    def translate(number)
+      result = ""
+      STRATEGIES.each do |strategy|
+        strategy.action(number, result) if strategy.match?(number)
+      end
+      return result
     end
   end
 end
